059f8a8e454a385837a025d3aa93739314fcc254
VeerleHobbelink <v.l.n.f.hobbelink@vu.nl>
2022-05-09 15:07:31 +0200

    Evaluation baselines

--- detailed_eval.py
+++ detailed_eval.py
@@ -3,75 +3,69 @@
 import pandas as pd
 
 
-
 def clean_data():
-    model_output = pd.read_csv("majority_baseline.tsv", sep="\t", encoding='latin-1') #"experiments/base_model/model_output.tsv"
-    model_output.columns=["word", "label", "prediction"]
-    #print(model_output)
+    model_output = pd.read_csv("random_baseline.tsv", sep="\t", encoding='utf8')
+    model_output.columns=["tweet", "label", "prediction"]
     model_output = model_output.dropna()
     print(model_output.head())
-    #model_output.rename(columns={"word", "label", "prediction"}, inplace=True)
-    #print(model_output)
     model_output.to_csv("model_out_maj.csv")
 
-
-
 if __name__ == '__main__':
-
     #Per class, per model: Precision, Recall, F1
-    # clean_data()
+    clean_data()
     model_output = pd.read_csv("model_out_maj.csv")
 
-    C = model_output[model_output["label"] == "C"]
-    print(C)
-   # C.to_csv("c.tsv")
-    N = model_output[model_output["label"] == "N"]
-    #N.to_csv("n.tsv")
-    print(N)
+    Off = model_output[model_output["label"] == 1]
+    Not = model_output[model_output["label"] == 0]
+
+    SizeOff = len(Off)
+    SizeNot = len(Not)
+
 
     #Precision = True Positives / (True Positives + False Positives)
 
-    trueposC = C[C["label"] == C["prediction"]].shape[0]
-    falseposC =  N[N["prediction"] == "C"].shape[0]
+    trueposOff = Off[Off["label"] == Off["prediction"]].shape[0]
+    falseposOff =  Not[Not["prediction"] == 1].shape[0]
 
-    trueposN = N[N["label"] == N["prediction"]].shape[0]
-    falseposN = C[C["prediction"] == "N"].shape[0]
-    print("TrueposC", trueposC)
-    print("FalseposC", falseposC)
+    trueposNot = Not[Not["label"] == Not["prediction"]].shape[0]
+    falseposNot = Off[Off["prediction"] == 0].shape[0]
 
-    if (trueposC + falseposC) == 0:
-        precisionC = 0
+    if (trueposOff + falseposOff) == 0:
+        precisionOff = 0
     else:
-        precisionC = trueposC / (trueposC + falseposC)
-    print("precisionC", precisionC)
+        precisionOff = trueposOff / (trueposOff + falseposOff)
 
-    precisionN = trueposN / (trueposN + falseposN)
-    print("N truepos, falsepos, precision", trueposN, falseposN, precisionN)
-
+    precisionNot = trueposNot / (trueposNot + falseposNot)
 
     #Recall = True Positives / (True Positives + False Negatives)
-    falsenegC = C[C["prediction"] == "N"].shape[0]
-    recallC = trueposC / (trueposC + falsenegC)
-    print("C truepos, falseneg, recall", trueposC, falsenegC, recallC)
+    falsenegOff = Off[Off["prediction"] == 0].shape[0]
+    recallOff = trueposOff / (trueposOff + falsenegOff)
 
-    falsenegN = N[N["prediction"] == "C" ].shape[0]
-    recallN = trueposN / (trueposN + falsenegN)
-    print("N truepos, falseneg, recall", trueposN, falsenegN, recallN)
+    falsenegNot = Not[Not["prediction"] == 1 ].shape[0]
+    recallNot = trueposNot / (trueposNot + falsenegNot)
 
      #F1 = 2 * (Precision * Recall) / (Precision + Recall)
-    if precisionC == 0 or recallC == 0:
-        F1C = 0
-    elif precisionC != 0 or recallC != 0:
-        F1C = 2 * (precisionC * recallC) / (precisionC + recallC)
+    if precisionOff == 0 or recallOff == 0:
+        F1Off = 0
+    elif precisionOff != 0 or recallOff != 0:
+        F1Off = 2 * (precisionOff * recallOff) / (precisionOff + recallOff)
 
-    if precisionN == 0 or recallN == 0:
-        F1N = 0
-    elif precisionN != 0 or recallN != 0:
-        F1N = 2 * (precisionN * recallN) / (precisionN + recallN)
+    if precisionNot == 0 or recallNot == 0:
+        F1Not = 0
+    elif precisionNot != 0 or recallNot != 0:
+        F1Not = 2 * (precisionNot * recallNot) / (precisionNot + recallNot)
 
-    print("Precision C = ", precisionC,  "  N = ", precisionN)
-    print("Recall C = ", recallC, " N = ", recallN)
-    print("F1C", F1C, "F1N", F1N)
+    macroavprecision = 0.5 * precisionOff + 0.5 * precisionNot
+    macroavrecall = 0.5 * recallOff + 0.5 * recallNot
+    macroavF1 = 0.5 * F1Off + 0.5 * F1Not
 
-    ## TO - DO weighthed average
+    totallen = SizeNot + SizeOff
+    weightedavprecision = SizeOff/totallen * precisionOff + SizeNot/totallen * precisionNot
+    weightedavrecall = SizeOff/totallen * recallOff + SizeNot/totallen * recallNot
+    weigtedavF1 = SizeOff/totallen * F1Off + SizeNot/totallen * F1Not
+
+    print("Precision Off = ", precisionOff,  "  Not = ", precisionNot, "Macro-average", macroavprecision, "Weighted average", weightedavprecision)
+    print("Recall Off = ", recallOff, " Not = ", recallNot, "Macro-average", macroavrecall, "Weighted average", weightedavrecall)
+    print("F1Off", F1Off, "F1Not", F1Not, "Macro-average", macroavF1, "Weighted average", weigtedavF1)
+
 
