2484b974f40ce73b6d4e7dbb6c27dc3a142152c3
Demi Kruijer <daekruijer@gmail.com>
2022-05-09 13:34:07 +0200

    Exercise 1

--- NLP_A2_taskA.py
+++ NLP_A2_taskA.py
@@ -1,2 +1,19 @@
 # NLP assignment 2
 # Task A
+
+import pandas as pd
+import csv
+
+# 1: Class distributions 
+with open('data/olid-train.csv') as csv_file:
+    test_data = pd.read_csv(csv_file)
+
+# print occurences of labels 
+print(test_data['labels'].value_counts())
+
+# compute relative frequencies 
+tot_len = len(test_data)
+rel_occ0 = test_data['labels'].value_counts()[0]/tot_len
+rel_occ1 = test_data['labels'].value_counts()[1]/tot_len
+print('freq 0:', rel_occ0, 'and freq 1:', rel_occ1)
+
